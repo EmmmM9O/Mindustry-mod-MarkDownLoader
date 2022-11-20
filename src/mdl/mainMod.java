6a955d74d6914ac315f7dd68c364900886add2a5
EmmmM9O <2124363741@qq.com>
2022-11-20 06:25:37 +0000

    master

--- src/mdl/mainMod.java
+++ src/mdl/mainMod.java
@@ -1,17 +1,14 @@
-package example;
+package mdl;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class mainMod extends Mod{
 
-    public ExampleJavaMod(){
+    public mainMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -20,8 +17,7 @@ public class ExampleJavaMod extends Mod{
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +26,8 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading mdl");
+
     }
 
 }

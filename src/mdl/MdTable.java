af12fc41dbe12f8cdf69dd4aa0b98ed2e7c43afc
EmmmM9O <2124363741@qq.com>
2022-11-20 08:10:07 +0000

    master

--- src/mdl/MdTable.java
+++ src/mdl/MdTable.java
@@ -1,18 +1,35 @@
 package mdl;
 
+import arc.graphics.Colors;
+import arc.scene.ui.Label;
 import arc.scene.ui.layout.Table;
+import mindustry.ui.Fonts;
 
+import java.util.Stack;
 import java.util.Vector;
 
 public class MdTable {
+    public Label.LabelStyle Xt=new Label.LabelStyle(Fonts.tech, Colors.get("WHITE"));
+    public Label.LabelStyle Ut=new Label.LabelStyle(Fonts.def,Colors.get("WHITE"));
     public Vector<String> arr=new Vector<>();
+    public Stack<Integer> XList=new Stack<Integer>();
     public void init(Table ta){
         String now="";
-        float nowf=1f;
+        var nowt=Ut;
+        float nowf=1f;int Xad=-1;
         for (String i:arr){
-            nowf=1f;now="";
+            nowf=1f;now="";nowt=Ut;
             for (int v=0;v<i.length();v++) {
                 char a = i.charAt(v);
+                if (a=='*'){
+                    var k=i.indexOf("*",v+1);
+                    if (k!=-1){
+                        Xad=k;
+                    }
+                }
+                if (v<=Xad){
+                    nowt=Xt;
+                }
                 if (a == ' ') {
                     if (now.matches("#+")) {
                         nowf = (float) (3.3f - now.length() * 0.3f);
@@ -21,12 +38,12 @@ public class MdTable {
                 } else now = now + a;
 
             }
-            if (now.length()!=0) ta.add(now,nowf);
+            if (now.length()!=0) ta.add(now,nowt,nowf);
             ta.row();
         }
     }
     public void reset(String data,Table t){
-
+        arr.clear();
         String now="";
         for(int i=0;i<data.length();i++){
             char a=data.charAt(i);

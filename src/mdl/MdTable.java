bd2c590724c1f14ed8f2e26911fca35724740b09
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-11-26 20:03:06 +0800

    Update MdTable.java

--- src/mdl/MdTable.java
+++ src/mdl/MdTable.java
@@ -17,7 +17,7 @@ public class MdTable {
     public static MdG key[]={Gs.XieTi};
     public static String k="[^*~#\\n]";
     public static Label.LabelStyle UseL=new Label.LabelStyle(Fonts.outline,Colors.get("Black"));
-    public Vector<Integer>[] l=new Vector[key.length]();
+    public Vector<Integer>[] l=new Vector[key.length];
     public Integer useScl=1;
     public Integer[] now=new Integer[key.length];
     public void init(){

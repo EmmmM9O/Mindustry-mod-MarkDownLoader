544004b840e1efd414039db64c036fe3703454b9
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-02 22:24:49 +0800

    Update Gs.java

--- src/mdl/Gs.java
+++ src/mdl/Gs.java
@@ -10,6 +10,7 @@ public class Gs {
     public static void init(){
         ChuTi.i="(\\b|^)\\*{1}\\b[^*\\n]+\\b\\*{1}(\\b|$)";
         ChuTi.Run=(t,scl,e)->{
+            t.add("ChuTiTest");
             e=C;
         };
 

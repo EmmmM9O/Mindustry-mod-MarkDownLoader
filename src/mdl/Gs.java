cfdc67046df09084a39ec2de5dbdd9f764c0f6e1
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-03 22:44:53 +0800

    Update Gs.java

--- src/mdl/Gs.java
+++ src/mdl/Gs.java
@@ -3,8 +3,7 @@ package mdl;
 import arc.Core;
 import arc.files.Fi;
 import arc.files.ZipFi;
-import arc.freetype.FreeTypeFontGenerator;
-import arc.freetype.FreetypeFontLoader;
+import arc.freetype.*;
 import arc.graphics.Color;
 import arc.graphics.g2d.Font;
 import arc.scene.ui.Label;
@@ -18,23 +17,43 @@ public class Gs {
     public static MdG ChuTi=new MdG(),XieTi=new MdG(),title=new MdG();
     public static void init(){
 
-        Fi fontFi = Vars.mods.getMod(mainMod.class).root.child("fonts").child("SmileySans-Oblique.ttf");
-		if (!fontFi.exists()) {
-			MSYHMONO = Fonts.def;
-			return;
-		}
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFi);
-		FreeTypeFontParameter parameter = new FreeTypeFontParameter() {{
-			size = 22;
-			shadowColor = Color.black;
-			shadowOffsetY = 2;
-			incremental = true;
-		}};
-		CT = generator.generateFont(parameter, new FreeTypeFontData() {
-			{
-				markupEnabled = true;
-			}
-		});
+        
+
+Fi fontFi = Vars.mods.getMod(mainMod.class).root.child("fonts").child("SmileySans-Oblique.ttf");
+
+if (!fontFi.exists()) {
+
+        CT = Fonts.def;
+
+        return;
+
+}
+
+FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFi);
+
+FreeTypeFontParameter parameter = new FreeTypeFontParameter() {{
+
+        size = 22;
+
+        shadowColor = Color.darkGray;
+
+        shadowOffsetY = 2;
+
+        incremental = true;
+
+}};
+
+// final boolean[] generating = {false};
+
+CT = generator.generateFont(parameter, new FreeTypeFontData() {
+
+{
+
+        markupEnabled = true;
+
+}
+
+});
         ChuTi.i="((?![^*])|^)\\*{1}\\b[^*\\n]+\\b\\*{1}((?=[^*])|$)";
         ChuTi.Run=(t,scl,e,s)->{
             t.add(s,C,scl);

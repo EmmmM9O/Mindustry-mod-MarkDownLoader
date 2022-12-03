b52cc250cd37b3a6f9afc9e7aa65a4a78eb01654
EmmmM9O <2124363741@qq.com>
2022-12-03 12:46:41 +0000

    master

--- src/mdl/Gs.java
+++ src/mdl/Gs.java
@@ -7,6 +7,7 @@ import arc.freetype.FreetypeFontLoader;
 import arc.graphics.Color;
 import arc.graphics.g2d.Font;
 import arc.scene.ui.Label;
+import mindustry.Vars;
 import mindustry.ui.Fonts;
 import arc.util.*;
 
@@ -15,7 +16,7 @@ public class Gs {
     public static Label.LabelStyle C=new Label.LabelStyle(CT, Color.black);
     public static MdG ChuTi=new MdG(),XieTi=new MdG(),title=new MdG();
     public static void init(){
-        CT=new Font(Fi.get("fonts/SourceHanSansCN-Normal.ttf"));
+        CT=new Font(Vars.mods.getMod(mainMod.class).root.child("fonts").child("SmileySans-Oblique.ttf"));
         ChuTi.i="((?![^*])|^)\\*{1}\\b[^*\\n]+\\b\\*{1}((?=[^*])|$)";
         ChuTi.Run=(t,scl,e,s)->{
             t.add(s,C,scl);

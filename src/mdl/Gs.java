ed309e925a39d70252bbcb50a2a35b3bd41f56d0
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-03 19:20:03 +0800

    Update Gs.java

--- src/mdl/Gs.java
+++ src/mdl/Gs.java
@@ -9,7 +9,7 @@ public class Gs {
     public static Label.LabelStyle C=new Label.LabelStyle(Fonts.tech, Color.black);
     public static MdG ChuTi=new MdG(),XieTi=new MdG(),title=new MdG();
     public static void init(){
-        ChuTi.i="(\\b|^)\\*{1}\\b[^*\\n]+\\b\\*{1}(\\b|$)";
+        ChuTi.i="((?![^*])|^)\\*{1}\\b[^*\\n]+\\b\\*{1}((?=[^*])|$)";
         ChuTi.Run=(t,scl,e,s)->{
             t.add(s,C,scl);
             e=C;

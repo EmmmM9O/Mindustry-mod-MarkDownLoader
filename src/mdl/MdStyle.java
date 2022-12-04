8aea1876d68139b410a3da5d5590578ed60423a5
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-04 08:55:07 +0800

    Update MdStyle.java

--- src/mdl/MdStyle.java
+++ src/mdl/MdStyle.java
@@ -6,8 +6,8 @@ import arc.scene.ui.Label;
 public class MdStyle {
     public static Label.LabelStyle Smiley,OsBlack,OsRi;
     public static void init(){
-        Smiley=new Label.LabelStyle(MdFonts.Smiley, Color.black);
-        OsBlack=new Label.LabelStyle(MdFonts.OsBlack,Color.black);
-        OsRi=new Label.LabelStyle(MdFonts.OsRI,Color.black);
+        Smiley=new Label.LabelStyle(MdFonts.Smiley, Color.darkGray);
+        OsBlack=new Label.LabelStyle(MdFonts.OsBlack,Color.darkGray);
+        OsRi=new Label.LabelStyle(MdFonts.OsRI,Color.darkGray);
     }
 }

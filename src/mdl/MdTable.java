a1ac954abc960a0dedaae438806d04c257548c50
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-08 22:24:04 +0800

    Update MdTable.java

--- src/mdl/MdTable.java
+++ src/mdl/MdTable.java
@@ -1,11 +1,13 @@
+ lines (73 sloc)  2.67 KB
+
 package mdl;
 
+
+import arc.Core;
 import arc.graphics.Color;
 
 import arc.scene.ui.Label;
-
 import arc.scene.ui.layout.Table;
-
 import arc.util.*;
 
 import java.util.Vector;
@@ -13,128 +15,93 @@ import java.util.Vector;
 import java.util.regex.Pattern;
 
 public class MdTable {
-
     public static MdG[] key={Gs.Yy,Gs.Te,Gs.Til,Gs.ChuTi,Gs.XieTi,Gs.TI};
-
-    public static String k="[^`>~#*\\n]+";
-
-    public static Label.LabelStyle UseL=new Label.LabelStyle(MdFonts.OsTh, Color.black);
-
+    public static String k="[^~#*\\n>`]+";
+    public static Label.LabelStyle UseL=new Label.LabelStyle(MdFonts.OsTh,mainMod.TiColor);
     public Vector<Integer>[] l=new Vector[key.length];
 
     public Integer[] now=new Integer[key.length];
-
     public void init(){
-
         for(var i=0;i<key.length;i++) {
-
             l[i] = new Vector<>();
-
         }
-
     }
-
     public void add(String data,Table t){
 
         float scl=1f;int cnt;
-
         Label.LabelStyle e=UseL;
 
+
         var ss=data.split("\\n");
-
         for (var nows : ss){
-
             for (var i:l){
-
                 if(i.size()>0) i.clear();
-
             }
-
             cnt=0;
-
             for (MdG i : key){
-
                 var m= Pattern.compile(i.i).matcher(nows);
 
                 while (m.find()){
-
                     l[cnt].add(m.start());
-
                     l[cnt].add(m.end());
-
                     Log.info("add");
                     Log.info(m.start());
                     Log.info(m.end());
-
                 }
-
                 cnt++;
-
             }
 
             for(int i=0;i<key.length;i++){
-
                 now[i]=0;
-
             }
-
             var m=Pattern.compile(k).matcher(nows);
-
             while (m.find()){
-
                 e=new Label.LabelStyle(UseL);
-
                 cnt=-1;
-
                 String ew=m.group();
 
-                for (var i:key){
 
+                for (var i:key){
                     cnt++;
 
                     if(now[cnt]*2>=l[cnt].size()) continue;
-
                     if(now[cnt]==null||l[cnt].get(now[cnt]*2)==null) continue;
-
                     if (m.end()<=l[cnt].get(now[cnt]*2+1)&&m.start()>=l[cnt].get(now[cnt]*2)){
 
                         var pa=i.Run.get(t,scl,new Label.LabelStyle(e),ew,nows,m.start());
-                      
                         scl=pa.first;e=new Label.LabelStyle(pa.second);
                         Log.info(scl);
-
                         
-
                     }else if(m.end()>l[cnt].get(now[cnt]*2+1)) {
-
                         now[cnt]++;
-                        Log.info("out");
-
                         e=new Label.LabelStyle(UseL);
-
+                        Log.info("end");
                     }
+                    Log.info("now:");
                     Log.info(now[cnt]);
-
                 }
                 
+                         
+                        
 
-                t.add(ew,e,scl);
-
+                t.add(ew,e,scl).size(ew.length()*42*scl);
+                e.background= Core.atlas.drawable("mdl-BackGround");
             }
-            
-
             t.row();
-
             scl=1f;
-
         }
 
-            
 
+            
     }
 
+
+
+
+
+
     public MdTable(){
-
 }
-
 }
+Footer
+

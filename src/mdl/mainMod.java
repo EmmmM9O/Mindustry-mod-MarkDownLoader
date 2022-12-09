a46a9a07c64bc184a57b19f400bece572dd2c93f
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-09 12:05:12 +0800

    Update mainMod.java

--- src/mdl/mainMod.java
+++ src/mdl/mainMod.java
@@ -8,9 +8,10 @@ import mindustry.mod.*;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.ui.dialogs.*;
-
+import arc.graphics.Color;
 public class mainMod extends Mod{
     public MarkBlock MarkDownMessage;
+    public static Color TiColor=new Color(0x202125ff);
     public mainMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
